97c4c399a89aef4525e9b8d5866bc11723ce2b96
Brett Curtis <brett@osinfra.io>
2024-06-29 16:39:07 -0400

    Disable Datadog tracing and profiling

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -11,25 +11,39 @@ import (
         // "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
         // "gopkg.in/DataDog/dd-trace-go.v1/profiler"
 
-        httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
+        // httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
 )
 
+// func main() {
+//         tracer.Start()
+//         defer tracer.Stop()
+
+//         err := profiler.Start(
+//                 profiler.WithProfileTypes(
+//                         profiler.CPUProfile,
+//                         profiler.HeapProfile,
+//                 ),
+//         )
+//         if err != nil {
+//                 log.Fatal(err)
+//         }
+//         defer profiler.Stop()
+
+//         mux := httptrace.NewServeMux()
+//         mux.HandleFunc("/gke-info", metadata.GetMetadata)
+
+//         port := "8080"
+//         if envPort := os.Getenv("PORT"); envPort != "" {
+//                 port = envPort
+//         }
+
+//         if err := http.ListenAndServe(fmt.Sprintf(":%s", port), mux); err != nil {
+//                 log.Fatalf("Failed to start server: %v", err)
+//         }
+// }
+
 func main() {
-        // tracer.Start()
-        // defer tracer.Stop()
-
-        // err := profiler.Start(
-        //         profiler.WithProfileTypes(
-        //                 profiler.CPUProfile,
-        //                 profiler.HeapProfile,
-        //         ),
-        // )
-        // if err != nil {
-        //         log.Fatal(err)
-        // }
-        // defer profiler.Stop()
-
-        mux := httptrace.NewServeMux()
+        mux := http.NewServeMux() // Use standard http.ServeMux
         mux.HandleFunc("/gke-info", metadata.GetMetadata)
 
         port := "8080"
