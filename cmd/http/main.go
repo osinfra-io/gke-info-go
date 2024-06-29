05f2fc376b5351197e808059a60ef779ba2d2a01
Brett Curtis <brett@osinfra.io>
2024-06-29 16:31:14 -0400

    Disable Datadog tracing and profiling (#15)

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -8,26 +8,26 @@ import (
 
         "gke-info/internal/metadata"
 
-        "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
-        "gopkg.in/DataDog/dd-trace-go.v1/profiler"
+        // "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+        // "gopkg.in/DataDog/dd-trace-go.v1/profiler"
 
         httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
 )
 
 func main() {
-        tracer.Start()
-        defer tracer.Stop()
+        // tracer.Start()
+        // defer tracer.Stop()
 
-        err := profiler.Start(
-                profiler.WithProfileTypes(
-                        profiler.CPUProfile,
-                        profiler.HeapProfile,
-                ),
-        )
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer profiler.Stop()
+        // err := profiler.Start(
+        //         profiler.WithProfileTypes(
+        //                 profiler.CPUProfile,
+        //                 profiler.HeapProfile,
+        //         ),
+        // )
+        // if err != nil {
+        //         log.Fatal(err)
+        // }
+        // defer profiler.Stop()
 
         mux := httptrace.NewServeMux()
         mux.HandleFunc("/gke-info", metadata.GetMetadata)
