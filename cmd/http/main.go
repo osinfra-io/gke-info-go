dae5a2f02a0617bccbc75e50d027d7407f55812d
Brett Curtis <brett@osinfra.io>
2024-08-25 13:14:34 -0400

    Add logging levels

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -48,9 +48,9 @@ func main() {
 	}
 
 	go func() {
-		log.Printf("Starting server on port %s...\n", port)
+		log.Printf("[INFO] Starting server on port %s...\n", port)
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Failed to start server: %v", err)
+			log.Fatalf("[ERROR] Failed to start server: %v", err)
 		}
 	}()
 
@@ -58,13 +58,13 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shutting down server...")
+	log.Println("[INFO] Shutting down server...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
+		log.Fatalf("[ERROR] Server forced to shutdown: %v", err)
 	}
 
-	log.Println("Server exiting")
+	log.Println("[INFO] Server exiting")
 }
